5fd7742ce5db0a25b7bd403de2742b4708ead3a3
David Dawes <davidd@remitly.com>
2018-07-07 01:16:41 -0700

    First bit of testing in.

--- test/spec.js
+++ test/spec.js
@@ -2,6 +2,7 @@ const Application = require('spectron').Application
 const assert = require('assert')
 const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
 const path = require('path')
+const chai = require('chai')
 describe('Application launch', function () {
   this.timeout(30000)
 
@@ -29,9 +30,20 @@ describe('Application launch', function () {
     })
   })
 
-  it('supports VerusCoin selection', function () {
-    this.app.client.getText("document.getElementsByClassName('Select-arrow')[1]").then(function (result) {
-      console.log('The Select-arrow selection returns ' + result)
+  it('has a native coin drop down that takes <Tab>, should be VRSC', function () {
+    // Wait for the left button for native mode coins is visible
+    this.app.client.element('#react-select-3--value').waitForVisible(3000)
+    // Click on it and hit <Tab> to select the 1st - VerusCoin
+    return this.app.client.element('#react-select-3--value').click().keys("Tab")
+  })
+
+  it('removes the coin selector', function() {
+    return this.app.client.element('#react-select-3--value').waitForVisible(4000, true)
+  })
+
+  it('shows a single window', function () {
+    return this.app.client.getWindowCount().then(function (count) {
+      assert.equal(count, 1)
     })
   })
 })
